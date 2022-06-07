51cd1a37271d0b65b8d5addca2e191b35acb91ad
msnaye <99111936+msnaye@users.noreply.github.com>
2022-06-06 22:34:30 -0500

    created req, res

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
+const fs = require('fs');
+const path = require ('path');
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
 const express = require('express'); 
 const db = require('./db/db.json');
+
 const app = express();
-const PORT = 4444;
+const PORT = process.env.PORT || 4444;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/') //for HTML FILE
+app.get('/', htmlRoutes);
+app.get('api', apiRoutes);
 
-
-
-
-app.listen(4444, () => {
-    console.log(`API server now on port 4444!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
