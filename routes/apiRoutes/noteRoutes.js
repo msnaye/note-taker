b199b179808ba9dcc9782679636dea5c774f3b70
msnaye <99111936+msnaye@users.noreply.github.com>
2022-06-06 23:58:23 -0500

    final commit

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,39 +2,24 @@ const fs = require('fs');
 const router = require('express').Router();
 const notes = require('../../db/db.json');
 const path = require('path');
-
+const randomId = require('random-id');
 
 router.get('/api/notes', (req, res)=>{
-    // let results = notes;
-    // if (re.query){
-    //     results = filterByQuery (req.query, results);
-    // }
-    // res.json(notes);
+    
     res.sendFile(path.join(__dirname, "../../db/db.json"));
 });
-// router.get('/notes/:title', (req, res) => {
-//     const result = findById(req.params.title, notes);
-//     if (result) {
-//       res.json(result);
-//     } else {
-//       res.send(404);
-//     }
-//   });
-//   router.post('/api/notes', (req, res) => {
-//     // set title based on what the next index of the array will be
-//     req.body.title = notes.length.toString();
 
-// // if any data in req.body is incorrect, send 400 error back
-// if (!validateNote(req.body)) {
-//     res.status(400).send('The note is not properly formatted.');
-// } else {
-
-//     // add note to json file and notes array in this function
-//     const note = createNewNote(req.body, notes);
-//     res.json(note);
-// }
-//   })
 //router.delete('/notes/:title')
- // });
 
+router.post('/api/notes', (req, res)=>{
+    const newNote = req.body;
+    const len = 10;
+    const pattern ='aA0';
+    newNote.id= randomId(len, pattern);
+    console.log(newNote)
+
+    notes.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+    res.json(notes)
+})
 module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -9,7 +9,6 @@ router.get("/notes",(req, res)=>{
     res.sendFile(path.join(__dirname, "../../public/notes.html"));
 })
 
-
 // router.get("*", (req, res) => {
 //     res.sendFile(path.join(__dirname, '../../public/index.html'));
 //   });
