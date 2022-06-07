6ca7892465428380019891c7aed05d600c3db29c
msnaye <99111936+msnaye@users.noreply.github.com>
2022-06-06 23:05:23 -0500

    fixed errors

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,36 +1,39 @@
+const fs = require('fs');
 const router = require('express').Router();
-const {notes} = require('../../data/notes');
+const notes = require('../../db/db.json');
 
 
 router.get('/notes', (req, res)=>{
-    let results = notes;
-    if (re.query){
-        results = filterByQuery (req.query, results);
-    }
-    res.json(notes);
+    // let results = notes;
+    // if (re.query){
+    //     results = filterByQuery (req.query, results);
+    // }
+    // res.json(notes);
+    res.sendFile(path.join(__dirname, "../../db/db.json"));
 });
-router.get('/notes/:title', (req, res) => {
-    const result = findById(req.params.title, notes);
-    if (result) {
-      res.json(result);
-    } else {
-      res.send(404);
-    }
-  });
-  router.post('/api/notes', (req, res) => {
-    // set title based on what the next index of the array will be
-    req.body.title = notes.length.toString();
+// router.get('/notes/:title', (req, res) => {
+//     const result = findById(req.params.title, notes);
+//     if (result) {
+//       res.json(result);
+//     } else {
+//       res.send(404);
+//     }
+//   });
+//   router.post('/api/notes', (req, res) => {
+//     // set title based on what the next index of the array will be
+//     req.body.title = notes.length.toString();
 
-// if any data in req.body is incorrect, send 400 error back
-if (!validateNote(req.body)) {
-    res.status(400).send('The note is not properly formatted.');
-} else {
+// // if any data in req.body is incorrect, send 400 error back
+// if (!validateNote(req.body)) {
+//     res.status(400).send('The note is not properly formatted.');
+// } else {
 
-    // add note to json file and notes array in this function
-    const note = createNewNote(req.body, notes);
-    res.json(note);
-}
-router.delete('/notes/:title')
-  });
+//     // add note to json file and notes array in this function
+//     const note = createNewNote(req.body, notes);
+//     res.json(note);
+// }
+//   })
+//router.delete('/notes/:title')
+ // });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const path = require ('path');
 
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes/noteRoutes');
+const htmlRoutes = require('./routes/htmlRoutes/index');
 
 
 const express = require('express'); 
-const db = require('./db/db.json');
+
 
 const app = express();
 const PORT = process.env.PORT || 4444;
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', htmlRoutes);
-app.get('api', apiRoutes);
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
